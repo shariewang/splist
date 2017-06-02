08c2c7e0106691effafcc96b7521e985181f98ac
Sharie Wang <Sharie Wang>
2017-06-02 01:06:53 -0700

    add new group fragment and menu

--- Splist/app/src/main/java/palie/splist/MainActivity.java
+++ Splist/app/src/main/java/palie/splist/MainActivity.java
@@ -1,9 +1,15 @@
 package palie.splist;
 
+import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+
+import com.google.firebase.auth.FirebaseAuth;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -25,4 +31,29 @@ public class MainActivity extends AppCompatActivity {
         GroupAdapter groupAdapter = new GroupAdapter(mGroups);
         recyclerView.setAdapter(groupAdapter);
     }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.main_menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.new_group:
+                getSupportFragmentManager().beginTransaction()
+                        .setTransition(FragmentTransaction.TRANSIT_FRAGMENT_OPEN).commit();
+                getSupportFragmentManager().beginTransaction()
+                        .add(R.id.main_content, new NewGroupFragment()).addToBackStack(null).commit();
+                return true;
+            case R.id.signOut:
+                FirebaseAuth.getInstance().signOut();
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+
 }
